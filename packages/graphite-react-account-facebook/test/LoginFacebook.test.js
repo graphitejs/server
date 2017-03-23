ffaf5faab21b641ce82a663f4f680a4062c101b2
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-03-23 10:25:05 -0300

    add unit test

--- packages/graphite-react-account-facebook/test/LoginFacebook.test.js
+++ packages/graphite-react-account-facebook/test/LoginFacebook.test.js
@@ -1,8 +1,35 @@
-import { expect } from 'chai';
-import LoginFacebook from '../src/LoginFacebook';
+import React from 'react';
+import { shallow } from 'enzyme';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+const { expect } = chai;
 
-describe('React Account LoginFacebook', () => {
-  it('Should be', () => {
-    expect(true).eql(true);
+import { LoginFacebook } from '../src/LoginFacebook';
+
+describe('<LoginFacebook />', () => {
+  var wrapper;
+
+  global.localStorage = {
+    getItem: sinon.spy(),
+    setItem: sinon.spy()
+  }
+
+  beforeEach(() => {
+    wrapper = shallow(<LoginFacebook />);
+  });
+
+  it('LoginFacebook should exist', () => {
+    expect(wrapper).to.exist;
+  });
+
+  it('should be instance of my component', () => {
+    const inst = wrapper.instance();
+    expect(inst).to.be.instanceOf(LoginFacebook);
+  });
+
+  it('Should be render button element', () => {
+    expect(wrapper.find('button')).to.exist;
   });
 });
