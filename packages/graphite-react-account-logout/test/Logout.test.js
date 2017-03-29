ac5b054468f3cb9a3e94dcc00f7eb9bb226565a4
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-03-29 10:04:49 -0300

    [UNIT-TEST] - testing component

--- packages/graphite-react-account-logout/test/Logout.test.js
+++ packages/graphite-react-account-logout/test/Logout.test.js
@@ -1,8 +1,35 @@
-import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+const { expect } = chai;
+
 import Logout from '../src/Logout';
 
-describe('React Logout', () => {
-  it('Should be', () => {
-    expect(true).eql(true);
+describe('<Logout />', () => {
+  var wrapper;
+
+  global.localStorage = {
+    getItem: sinon.spy(),
+    setItem: sinon.spy()
+  }
+
+  beforeEach(() => {
+    wrapper = shallow(<Logout />);
+  });
+
+  it('Logout should exist', () => {
+    expect(wrapper).to.exist;
+  });
+
+  it('should be instance of my component', () => {
+    const inst = wrapper.instance();
+    expect(inst).to.be.instanceOf(Logout);
+  });
+
+  it('Should be render button element', () => {
+    expect(wrapper.find('button')).to.exist;
   });
 });
