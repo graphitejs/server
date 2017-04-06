eb83bf89bca1acdd57c6995fbc946f49c21c1652
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-06 10:39:47 -0300

    [UNIT-TEST] - mongoose account password verify attr

--- packages/graphite-mongoose-account-password/test/AccountPassword.test.js
+++ packages/graphite-mongoose-account-password/test/AccountPassword.test.js
@@ -2,7 +2,14 @@ import { expect } from 'chai';
 import AccountPassword from '../src/AccountPassword';
 
 describe('Mongoose Account Password', () => {
-  it('Should be', () => {
-    expect(true).eql(true);
+  let accountPassword;
+
+  beforeEach(() => {
+    accountPassword = new AccountPassword();
+  });
+
+  it('Should be has attribute logger and also has to be a function', () => {
+    const { logger } = accountPassword;
+    expect(typeof logger === 'function').eql(true);
   });
 });
