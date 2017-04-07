5bc08d9bd4b6f1c39eec87c4741909bce9c53a57
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-07 10:09:19 -0300

    [UNIT-TEST] - Account password

--- packages/graphite-mongoose-account-password/test/AccountPassword.test.js
+++ packages/graphite-mongoose-account-password/test/AccountPassword.test.js
@@ -3,6 +3,7 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 const { expect } = chai;
+import bcrypt from 'bcrypt';
 import AccountPassword from '../src/AccountPassword';
 
 describe('Mongoose Account Password', () => {
@@ -25,9 +26,43 @@ describe('Mongoose Account Password', () => {
   });
 
   context('when execute findAccount', () => {
-    it('Should be execute this.Model.find', () => {
+    it('Should be execute this.Model.find', (done) => {
       const result = accountPassword.findAccount("123");
       expect(accountPassword.Model.findOne).to.have.been.called;
+      done();
+    });
+  });
+
+  context('when comparePassword', () => {
+    context('when the password is incorrect', () => {
+      it('Should be return false', (done) => {
+        const result = accountPassword.comparePassword("123456", "aaaaaaaaaaaa");
+        expect(result).to.be.false;
+        done();
+      });
+    });
+
+    context('when the password is correct', () => {
+      it('Should be return true', (done) => {
+        bcrypt.hash("123456", 10, function(err, hash) {
+          const result = accountPassword.comparePassword("123456", hash);
+          expect(result).to.be.true;
+          done();
+        });
+      });
+    });
+  });
+
+  context('when formatCallback', () => {
+    it('Should be return object with loginToken, loginTokenExpires and userId', (done) => {
+      const user = { userId: '123456' };
+      const SECRET = "SECRET";
+      const EXPIRES_IN = 10000;
+
+      const result = accountPassword.formatCallback(user, SECRET, EXPIRES_IN);
+      expect(result).to.be.object;
+      expect(result).to.include.keys('loginToken', 'loginTokenExpires', 'userId');
+      done();
     });
   });
 });
