f2a90a522b37a5fa074cade6817f50811878bead
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-11 10:03:15 -0300

    [UNIT-TEST] - up coverage, mongoose account facebook

--- packages/graphite-mongoose-account-facebook/test/AccountFacebook.test.js
+++ packages/graphite-mongoose-account-facebook/test/AccountFacebook.test.js
@@ -1,8 +1,23 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+const { expect } = chai;
+
+import { Facebook } from 'fb';
 import AccountFacebook from '../src/AccountFacebook';
 
 describe('Mongoose Account Facebook', () => {
-  it('Should be', () => {
-    expect(true).eql(true);
+  const appId = '12345';
+  const secret = '123456';
+  const redirect = 'http://localhost';
+  let accountFacebook;
+
+  beforeEach(() => {
+    accountFacebook = new AccountFacebook(appId, secret, redirect);
+  });
+
+  it('Should be include this keys redirect, appId, secret, facebook, url', () => {
+    expect(accountFacebook).to.include.keys('redirect', 'appId', 'secret', 'facebook', 'url');
   });
 });
