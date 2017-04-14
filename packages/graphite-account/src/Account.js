a159b98f4acd999675ee06ed3ce5144acee061c8
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-14 16:30:37 -0300

    [REFACTOR] - logger

--- packages/graphite-account/src/Account.js
+++ packages/graphite-account/src/Account.js
@@ -1,35 +1,36 @@
 import debug from 'debug';
-const logger = debug('Account');
 
 class Account {
+  logger = debug('Account');
+
   loggedIn(loginToken, loginTokenExpires, userId) {
     try {
-      logger('Init loggedIn');
-      logger(`loginToken: ${loginToken} - loginTokenExpires: ${loginTokenExpires} - userId: ${userId}`);
+      this.logger('Init loggedIn');
+      this.logger(`loginToken: ${loginToken} - loginTokenExpires: ${loginTokenExpires} - userId: ${userId}`);
       localStorage.setItem('Graphite.loginToken', loginToken);
       localStorage.setItem('Graphite.loginTokenExpires', loginTokenExpires);
       localStorage.setItem('Graphite.userId', userId);
-      logger('Save in localStorage');
+      this.logger('Save in localStorage');
       const event = new CustomEvent('loggedIn', { detail: { loginToken, loginTokenExpires, userId } });
       window.dispatchEvent(event);
-      logger('Trigger event');
-    } catch (e) {
-      logger('Error loggedIn: ', e);
+      this.logger('Trigger event');
+    } catch (error) {
+      this.logger(`Error loggedIn: ${error.message}`);
     }
   }
 
   logout() {
     try {
-      logger('Init logout');
+      this.logger('Init logout');
       localStorage.removeItem('Graphite.loginToken');
       localStorage.removeItem('Graphite.userId');
       localStorage.removeItem('Graphite.loginTokenExpires');
-      logger('Remove in localStorage');
+      this.logger('Remove in localStorage');
       const event = new Event('logout');
       window.dispatchEvent(event);
-      logger('Trigger event');
-    } catch (e) {
-      logger('Error logout: ', e);
+      this.logger('Trigger event');
+    } catch (error) {
+      this.logger(`Error logout: ${error.message}`);
     }
   }
 }
