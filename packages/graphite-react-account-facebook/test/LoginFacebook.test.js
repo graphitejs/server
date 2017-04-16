162843a37557971c6c57b17ae83f26d456e3a3c0
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-16 11:54:56 -0300

    [UNIT-TEST] - up coverage

--- packages/graphite-react-account-facebook/test/LoginFacebook.test.js
+++ packages/graphite-react-account-facebook/test/LoginFacebook.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
@@ -9,27 +9,29 @@ const { expect } = chai;
 import { LoginFacebook } from '../src/LoginFacebook';
 
 describe('<LoginFacebook />', () => {
-  var wrapper;
+  let wrapper;
 
   global.localStorage = {
     getItem: sinon.spy(),
-    setItem: sinon.spy()
-  }
+    setItem: sinon.spy(),
+  };
 
   beforeEach(() => {
-    wrapper = shallow(<LoginFacebook />);
+    wrapper = mount(<LoginFacebook />);
   });
 
-  it('LoginFacebook should exist', () => {
-    expect(wrapper).to.exist;
-  });
+  context('when create instance', () => {
+    it('LoginFacebook should exist', () => {
+      expect(wrapper).to.exist;
+    });
 
-  it('should be instance of my component', () => {
-    const inst = wrapper.instance();
-    expect(inst).to.be.instanceOf(LoginFacebook);
-  });
+    it('should be instance of my component', () => {
+      const inst = wrapper.instance();
+      expect(inst).to.be.instanceOf(LoginFacebook);
+    });
 
-  it('Should be render button element', () => {
-    expect(wrapper.find('button')).to.exist;
+    it('Should be render button element', () => {
+      expect(wrapper.find('button')).to.exist;
+    });
   });
 });
