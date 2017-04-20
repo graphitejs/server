86ca147d206621a4d80e151f96e1349e796aed34
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-20 17:33:29 -0300

    [UPDATE] - action create student

--- examples/school/views/StudentCreate.js
+++ examples/school/views/StudentCreate.js
@@ -1,14 +1,44 @@
 import { Component } from 'react';
+import { graphql } from 'react-apollo';
 import Formsy from 'formsy-react';
+import Input from '../components/Input';
+import { create } from '../graphql/students';
 
-export default class StudentCreate extends Component {
+class StudentCreate extends Component {
   constructor() {
     super();
+
+    this.state = {
+      canSubmit: false,
+    };
   }
 
   render() {
+    const { canSubmit } = this.state;
+
     return (
-      <h1>Form</h1>
+        <Formsy.Form onValidSubmit={this.submit.bind(this)} onValid={this.enableButton.bind(this)} onInvalid={this.disableButton.bind(this)} >
+          <Input name="name" title="Name" validationError="This is not a valid name" required />
+          <Input name="street" title="Street" validationError="This is not a valid street" required />
+          <button type="submit" disabled={!canSubmit}>Submit</button>
+        </Formsy.Form>
     );
   }
+
+  disableButton() {
+    this.setState({ canSubmit: false });
+  }
+
+  enableButton() {
+    this.setState({ canSubmit: true });
+  }
+
+  async submit(model) {
+    try {
+      const { data } = await this.props.mutate({ variables: { newSchool: model }});
+    } catch (e) {
+    }
+  }
 }
+
+export default graphql(create)(StudentCreate);
