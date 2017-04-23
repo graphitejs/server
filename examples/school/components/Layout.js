303aca27dc60c11ddc3bcb806f4d4f4a449961d3
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-23 18:15:09 -0300

    [CREATE] - new name for Layout component

---
+++ examples/school/components/Layout.js
@@ -0,0 +1,21 @@
+import { PropTypes } from 'react';
+import Header from './Header';
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD',
+};
+
+const Layout = (props) => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.children}
+  </div>
+);
+
+Layout.propTypes = {
+  children: PropTypes.element,
+};
+
+export default Layout;
