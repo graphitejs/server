bdbcb8396bce389f35ea09cef7954b9db0dc2ff2
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-24 13:49:50 -0300

    [UPDATE] - delete view delete

--- examples/school/pages/school.js
+++ examples/school/pages/school.js
@@ -1,14 +1,12 @@
 import Layout from '../components/Layout.js';
 import SchoolList from '../views/SchoolList';
 import SchoolCreate from '../views/SchoolCreate';
-import SchoolRemove from '../views/SchoolRemove';
 import withData from '../lib/withData';
 
 const Index = withData(() => (
   <Layout>
     <SchoolList />
     <SchoolCreate />
-    <SchoolRemove />
   </Layout>
 ));
 
