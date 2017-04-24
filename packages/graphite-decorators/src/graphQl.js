f0f2972b619afd8a3c6d1e2df3c92a582ebd92d4
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-24 13:51:59 -0300

    [UPDATE] - change attr name for nameClass

--- packages/graphite-decorators/src/graphQl.js
+++ packages/graphite-decorators/src/graphQl.js
@@ -10,13 +10,13 @@ import {
 import { functionName } from '@graphite/utils';
 
 const graphQl = function(target) {
-  const name = functionName(target);
+  const nameClass = functionName(target);
   const typesAttr = get(target.prototype, 'Types', '');
-  target.prototype.name = name;
+  target.prototype.nameClass = nameClass;
 
 
   target.prototype.Types = `
-    type ${name} {
+    type ${nameClass} {
       _id: String,
       ${typesAttr}
     }
@@ -31,23 +31,23 @@ const graphQl = function(target) {
 
   if (target.prototype.hasOne) {
     const hasOne = {};
-    hasOne[name] = target.prototype.hasOne;
+    hasOne[nameClass] = target.prototype.hasOne;
     Object.assign(target.prototype.Resolvers, hasOne);
   }
 
   if (target.prototype.hasMany) {
     const hasMany = {};
-    hasMany[name] = target.prototype.hasMany;
+    hasMany[nameClass] = target.prototype.hasMany;
     Object.assign(target.prototype.Resolvers, hasMany);
   }
 
   if (target.prototype.create || target.prototype.update || target.prototype.remove) {
     const responseTypes = {};
-    responseTypes[`response${name}`] = {};
-    responseTypes[`response${name}`][name.toLowerCase()] = function(model = {}) {
+    responseTypes[`response${nameClass}`] = {};
+    responseTypes[`response${nameClass}`][nameClass.toLowerCase()] = function(model = {}) {
       return isEmpty(pick(model, Object.keys(get(target.prototype, 'schema', {})))) ? null : model;
     };
-    responseTypes[`response${name}`].errors = function(errors) {
+    responseTypes[`response${nameClass}`].errors = function(errors) {
       const requiredKeys = ['key', 'message'];
       if (isArray(errors) && isEqual(keys(errors[0]), requiredKeys)) {
         return errors;
@@ -57,8 +57,8 @@ const graphQl = function(target) {
     Object.assign(target.prototype.Resolvers, responseTypes);
 
     target.prototype.Types += `
-      type response${name} {
-        ${name.toLowerCase()}: ${name},
+      type response${nameClass} {
+        ${nameClass.toLowerCase()}: ${nameClass},
         errors: [Errors],
       }
     `;
@@ -66,27 +66,27 @@ const graphQl = function(target) {
 
   if (target.prototype.create) {
     target.prototype.Types += `
-      input create${name} {
+      input create${nameClass} {
         ${target.prototype.createTypes}
       }
     `;
     target.prototype.Mutation = `${get(target.prototype, 'Mutation', '')}
-                                 ${target.prototype.create(name)}`;
+                                 ${target.prototype.create(nameClass)}`;
   }
 
   if (target.prototype.update) {
     target.prototype.Types += `
-      input update${name} {
+      input update${nameClass} {
         ${target.prototype.updateTypes}
       }
     `;
     target.prototype.Mutation = `${get(target.prototype, 'Mutation', '')}
-                                 ${target.prototype.update(name)}`;
+                                 ${target.prototype.update(nameClass)}`;
   }
 
   if (target.prototype.remove) {
     target.prototype.Mutation = `${get(target.prototype, 'Mutation', '')}
-                                 ${target.prototype.remove(name)}`;
+                                 ${target.prototype.remove(nameClass)}`;
   }
 
   target.prototype.Mutation = `${get(target.prototype, 'Mutation', '')}`;
