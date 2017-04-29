0e9cafce53f7cc2962462122c82099588566af59
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-04-29 13:16:36 -0300

    [FIX] - one to many and many to one is ready, add logger

--- packages/graphite-mongoose/src/crud/update.js
+++ packages/graphite-mongoose/src/crud/update.js
@@ -1,6 +1,8 @@
-import { keys, capitalize, get, values, difference } from 'lodash';
+import { keys, upperFirst, get, values, difference, lowerFirst } from 'lodash';
 import path from 'path';
 import DynRequire from 'dyn-require';
+import debug from 'debug';
+const logger = debug('mongoose');
 
 const updateModel = async function(_, data) {
   try {
@@ -13,29 +15,67 @@ const updateModel = async function(_, data) {
     const modules = new DynRequire(`${rootPath}/models`);
     const oldModel = await this.Model.findById(modelId);
     const modelUpdated = await this.Model.findByIdAndUpdate(modelId, model, { 'new': true });
+    logger(`Update model ${upperFirst(this.nameClass)}, id ${modelId}`);
     const updateItem = {};
-    updateItem[this.nameClass.toLowerCase()] = modelUpdated._id;
+    updateItem[lowerFirst(this.nameClass)] = modelUpdated._id;
 
     keysHasMany.forEach(async (keyHasMany) => {
-      const modelModule = modules.require(capitalize(keyHasMany));
-      const arrItems = modelUpdated[keyHasMany];
-      await modelModule.default.Model.update({ _id: { $in: arrItems }}, { $set: updateItem }, { multi: true });
-
-      updateItem[this.nameClass.toLowerCase()] = null;
       const diffItems = difference(oldModel[keyHasMany], modelUpdated[keyHasMany]);
-      diffItems.forEach(async (_id) => {
-        await modelModule.default.Model.update({ _id }, { $set: updateItem });
-      });
+      const modelModule = modules.require(upperFirst(keyHasMany));
+      const arrItems = modelUpdated[keyHasMany];
+
+      if (get(modelModule.default.hasMany, lowerFirst(this.nameClass), false)) {
+        logger(`Update ${upperFirst(keyHasMany)}, relation many[${upperFirst(keyHasMany)}] to many[${upperFirst(this.nameClass)}]`);
+      }
+
+      if (get(modelModule.default.hasOne, lowerFirst(this.nameClass), false)) {
+        logger(`Update ${upperFirst(keyHasMany)}, relation many[${upperFirst(keyHasMany)}] to one[${upperFirst(this.nameClass)}]`);
+        try {
+          await modelModule.default.Model.update({ _id: { $in: arrItems }}, { $set: updateItem }, { multi: true });
+          logger(`Update ${upperFirst(keyHasMany)}, id: ${arrItems.join(' ')} - parameters ${JSON.stringify(updateItem)}`);
+        } catch (error) {
+          logger(`Error ${e}`);
+        }
+
+        updateItem[lowerFirst(this.nameClass)] = null;
+        logger(`Update remove relation ${upperFirst(this.nameClass)} to ${upperFirst(keyHasMany)} ${diffItems.join(' ')}`);
+        diffItems.forEach(async (_id) => {
+          try {
+            await modelModule.default.Model.update({ _id }, { $set: updateItem });
+            logger(`Update ${upperFirst(keyHasMany)}, id: ${diffItems.join(' ')} - parameters ${JSON.stringify(updateItem)}`);
+          } catch (error) {
+            logger(`Error ${error}`);
+          }
+        });
+      }
     });
 
     keysHasOne.forEach(async (keyHasOne) => {
-      const modelModule = modules.require(capitalize(keyHasOne));
+      const modelModule = modules.require(upperFirst(keyHasOne));
       const item = modelUpdated[keyHasOne];
-      await modelModule.default.Model.update({ _id: item }, { $set: updateItem });
 
-      updateItem[this.nameClass.toLowerCase()] = item;
-      if (oldModel[keyHasOne] !== modelUpdated[keyHasOne]) {
-        await modelModule.default.Model.update({ _id: oldModel[keyHasOne]._id }, { $pull: updateItem });
+      if (get(modelModule.default.hasMany, lowerFirst(this.nameClass), false)) {
+        logger(`Update ${upperFirst(keyHasOne)}, relation many[${upperFirst(keyHasOne)}] to one[${upperFirst(this.nameClass)}]`);
+        try {
+          await modelModule.default.Model.update({ _id: item }, { $set: updateItem });
+          logger(`Update ${upperFirst(keyHasOne)}, id: ${item} - parameters ${JSON.stringify(updateItem)}`);
+        } catch (error) {
+          logger(`Error ${error}`);
+        }
+
+        updateItem[lowerFirst(this.nameClass)] = item;
+        if (oldModel[keyHasOne] !== modelUpdated[keyHasOne]) {
+          try {
+            await modelModule.default.Model.update({ _id: oldModel[keyHasOne]._id }, { $pull: updateItem });
+            logger(`Update ${upperFirst(keyHasOne)} - parameters ${JSON.stringify(updateItem)}`);
+          } catch (error) {
+            logger(`Error ${error}`);
+          }
+        }
+      }
+
+      if (get(modelModule.default.hasOne, lowerFirst(this.nameClass), false)) {
+        logger(`Update ${upperFirst(keyHasOne)}, relation one[${upperFirst(keyHasOne)}] to one[${upperFirst(this.nameClass)}]`);
       }
     });
 
