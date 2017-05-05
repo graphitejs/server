274bbff1dbef2c02eac29bdc48524e488bc14f88
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-05-04 23:43:00 -0300

    [FIX] - change about

--- examples/school/pages/about.js
+++ examples/school/pages/about.js
@@ -1,7 +1,39 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import Layout from '../components/Layout.js';
+import withData from '../lib/withData';
 
-export default () => (
-  <Layout>
-      <p>This is the about page</p>
-  </Layout>
-);
+class About extends Component {
+  static propTypes = {
+    items: PropTypes.array,
+  }
+
+  static contextTypes = {
+    store: PropTypes.object,
+  }
+
+  static defaultProps = {
+    items: [],
+  }
+
+  constructor() {
+    super();
+  }
+
+  static async getInitialProps({ store }) {
+    const { adminGraphite } = store.getState();
+    return { items: adminGraphite.items };
+  }
+
+  render() {
+    const { items } = this.props;
+
+    return (
+      <Layout items={items}>
+         <p>This is the about page</p>
+      </Layout>
+    );
+  }
+}
+
+export default withData(About);
