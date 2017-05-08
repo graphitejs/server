27b0316cf381c46a55945744a204c32a0f803230
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-05-08 11:26:03 -0300

    [UPDATE] - add behavior listDisplay

--- examples/school/server/main.js
+++ examples/school/server/main.js
@@ -128,13 +128,20 @@ app.prepare().then(async () => {
 
   const graphqlQuerys = [ School, Student, Teacher ].map(model => {
     const schemaModel = Object.keys(model.schema);
+    console.log("schemaModel.listDisplay ", model.listDisplay);
     schemaModel.unshift('_id');
     const hasManyKeys = Object.keys(get(model, 'hasMany', {}) );
     const hasOneKeys = Object.keys(get(model, 'hasOne', {}) );
     const hasManyDiffKeys = intersection(schemaModel, hasManyKeys);
     const hasOneDiffKeys = intersection(schemaModel, hasOneKeys);
     const newSchema = without(schemaModel, ...hasManyDiffKeys, ...hasOneDiffKeys);
-    const fields = newSchema.join(' ');
+    console.log("newSchema ",newSchema);
+    if(model.listDisplay) {
+      console.log("entro");
+      console.log("intersection(newSchema, model.listDisplay).join(' ') ",intersection(newSchema, model.listDisplay));
+    }
+    const fields = model.listDisplay ? intersection(newSchema, model.listDisplay) : newSchema.join(' ');
+    console.log("fields ",fields);
     const obj = {};
 
     forEachObject(model.schema, data => {
