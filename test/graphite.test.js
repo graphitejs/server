6710dbf4bdede47d0a780aabde4c431794f36a69
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-12-28 18:25:16 -0300

    remove stuff

--- test/graphite.test.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { Graphite } from '../src/Graphite';
-
-describe('Graphite', () => {
-  it('should be has property graphQLServer', (done) => {
-    expect(Graphite).to.include.keys('graphQLServer');
-    done();
-  });
-});
