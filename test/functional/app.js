a3a78177750e22103fbd2b0ca5e1d85a9df2c1c9
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-12-29 17:29:03 -0300

    fix lint

--- test/functional/app.js
+++ test/functional/app.js
@@ -5,24 +5,25 @@ import { Graphite } from '../../src/index'
 describe('Running Graphite', async assert => {
   {
     const graphite = await Graphite()
-  
+
     const options = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ query: '{ hello }' }),
     }
-  
+
     const request = await fetch('http://localhost:4000/graphql', options)
+
     const actual = (await request.json()).data.hello
     const expected = 'Hello World! 🎉🎉🎉'
-  
+
     assert({
       given: 'Graphite Server with any configuration',
       should: 'return the default query',
       actual,
       expected,
     })
-  
+
     await graphite.stop()
   }
 })
