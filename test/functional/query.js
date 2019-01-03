9e71291bbbf566a726e652bca0df83f1afee4464
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-01-03 12:43:46 -0300

    feature

--- test/functional/query.js
+++ test/functional/query.js
@@ -1,33 +1,48 @@
 import fetch from 'isomorphic-fetch'
 import { describe } from 'riteway'
 import { Graphite, GraphQL } from '../../src/index'
+import { getQuery } from '../Helpers/GetQuery'
 
+const Developer = GraphQL('Developer')({
+  name: ['String'],
+
+  Query: {
+    'developer: Developer': () => ({ name: 'Wally' }),
+    'getDeveloper(name: String): Developer': () => ({ name: 'Wally' }),
+  },
+})
 
 describe('Graphite Query', async assert => {
   {
-    const Developer = GraphQL('Developer')({
-      name: ['String'],
-
-      Query: {
-        'developer: Developer': () => ({ name: 'Wally' }),
-      },
-    })
-
     const graphite = await Graphite({ models: [Developer] })
 
-    const options = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query: '{ developer { name } }' }),
-    }
-
-    const request = await fetch('http://localhost:4000/graphql', options)
+    const query = getQuery('developer { name }')
+    const request = await fetch('http://localhost:4000/graphql', query)
 
     const actual = (await request.json()).data.developer.name
     const expected = 'Wally'
 
     assert({
       given: 'GraphQl query valid',
+      should: 'return the data query',
+      actual,
+      expected,
+    })
+
+    await graphite.stop()
+  }
+
+  {
+    const graphite = await Graphite({ models: [Developer] })
+
+    const query = getQuery('getDeveloper(name: "Wally") { name }')
+    const request = await fetch('http://localhost:4000/graphql', query)
+
+    const actual = (await request.json()).data.getDeveloper.name
+    const expected = 'Wally'
+
+    assert({
+      given: 'GraphQl query with params',
       should: 'return the data query',
       actual,
       expected,
