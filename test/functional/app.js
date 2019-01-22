f514595e49a585ca930d9583d5ac2752c88265d8
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-01-22 14:01:32 -0300

    feat: Configure Port (#71)

--- test/functional/app.js
+++ test/functional/app.js
@@ -2,16 +2,15 @@ import fetch from 'isomorphic-fetch'
 import { describe } from 'riteway'
 import { Graphite } from '../../src/index'
 
+const options = {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query: '{ hello }' }),
+}
+
 describe('Running Graphite', async assert => {
   {
     const graphite = await Graphite()
-
-    const options = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query: '{ hello }' }),
-    }
-
     const request = await fetch('http://localhost:4000/graphql', options)
 
     const actual = (await request.json()).data.hello
@@ -19,6 +18,23 @@ describe('Running Graphite', async assert => {
 
     assert({
       given: 'Graphite Server with any configuration',
+      should: 'return the default query',
+      actual,
+      expected,
+    })
+
+    await graphite.stop()
+  }
+
+  {
+    const graphite = await Graphite({ port: 3000 })
+    const request = await fetch('http://localhost:3000/graphql', options)
+
+    const actual = (await request.json()).data.hello
+    const expected = 'Hello World! 🎉🎉🎉'
+
+    assert({
+      given: 'Graphite Server with another port',
       should: 'return the default query',
       actual,
       expected,
